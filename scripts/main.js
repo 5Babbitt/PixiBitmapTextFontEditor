d3ce118c9ce086c1d6ed2a672c50097993b424b3
Owen <owen.harbert2@gmail.com>
2025-07-02 01:19:04 +0200

    move assets and center component

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /**
  * [ ] Load and display bitmapText
  */
@@ -16,16 +17,25 @@ import { Application, Assets, BitmapText } from "pixi.js"
   	canvas.appendChild(app.view)
 
 	await Assets.load('/FNTBaseBonus01a.xml').then(() => {
-		const bitmapFontText = new BitmapText('0123', {
+		const bitmapFontText = new BitmapText('0123456789,.', {
 			fontName: 'FNTBaseBonus01a',
-			fontSize: 55,
+			fontSize: 36,
 			align: "center"
 		})
 
   		bitmapFontText.x = 50;
   		bitmapFontText.y = 200;
 
-		app.stage.addChild(bitmapFontText)
+		app.stage.addChild(centerComponent(bitmapFontText, app))
 	})
-
 }) ()
+
+function centerComponent(component, app) {
+	component.pivot.x = component.width / 2
+	component.pivot.y = component.height / 2
+
+	component.x = app.screen.width / 2
+	component.y = app.screen.height / 2
+
+	return component
+}
