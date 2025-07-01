7ce9017ed5bef1d7607b6cbbaf36ebc80a5832bc
Owen <owen.harbert2@gmail.com>
2025-07-02 01:13:16 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PixiBitmapTextFontEditor/',
   server: {
     port: 8080,
     open: true,
